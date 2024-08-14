c190a878834409e772140324ce369509d6567392
Ulisses Batista Santana <santana.gru@gmail.com>
2024-08-14 20:47:41 -0300

    ControllerAdvice e ExceptionHandler

---
+++ src/main/java/com/events/desafio/controller/handles/ControllerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.events.desafio.controller.handles;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.events.desafio.dto.CustomErrorDTO;
+import com.events.desafio.service.exception.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorDTO> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+   
+}
